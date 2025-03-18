975f35f19887673e650059a3346ca0778d0b6f6b
Juan Mendez <Juan.mendez1@grainger.com>
2025-03-17 19:22:13 -0500

    chapter0

--- SwiftUiAdvanced/SwiftUiAdvanced/Views/Experiments/ZStackDemo1.swift
+++ SwiftUiAdvanced/SwiftUiAdvanced/Views/Experiments/ZStackDemo1.swift
@@ -9,17 +9,22 @@ import SwiftUI
 
 struct ZStackDemo1: View {
     var body: some View {
-        ZStack(alignment: .topLeading) {
+        ZStack(alignment: .top) {
             Rectangle()
-                .fill(.red)
-                .frame(width: 100, height: 100)
+                .fill(Color.red)
+                .frame(width: 200, height: 200)
+                .alignmentGuide(.leading) {
+                    d in d[.leading]
+                }
+                .alignmentGuide(.top) {
+                    d in d[.top]
+                }
             
+            Text("hello world")
+                .offset(x: -60, y:0)
             
-            Rectangle()
-                .fill(.orange)
-                .frame(width: 100, height: 100)
-            
-            
+            Text("hello world2")
+                .offset(x: 50, y:180)
         }
     }
 }
